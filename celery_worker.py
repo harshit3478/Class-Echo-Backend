3012186cfd559e053cc7dc1186d024b28fa00d81
harshit3478 <harsagra3478@gmail.com>
2026-04-03 15:47:57 +0530

    Fix Celery 6.0 broker_connection_retry_on_startup depreciation warning

--- celery_worker.py
+++ celery_worker.py
@@ -10,6 +10,7 @@ celery_app = Celery(
 
 celery_app.conf.update(
     worker_concurrency=2,
+    broker_connection_retry_on_startup=True,
     task_serializer="json",
     result_serializer="json",
     accept_content=["json"],
