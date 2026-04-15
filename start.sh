715757321446d4d92baf1c93f72270074275eef3
harshit3478 <harsagra3478@gmail.com>
2026-04-15 16:49:25 +0530

    last try

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ alembic upgrade head
 python seed_admin.py
 
 # Start celery worker in the background
-celery -A celery_worker.celery_app worker --loglevel=info --concurrency=2 &
+celery -A celery_worker.celery_app worker --loglevel=info --concurrency=1 &
 # Start uvicorn
 uvicorn app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
